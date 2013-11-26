ab184c4b56f71516ae5376c9743f55a194d29e56
Mario Retana <maretana@ic-itcr.ac.cr>
2013-11-25 22:03:40 -0600

    Segundo avanze con el shellscript.

--- BusquedaHadoop/busc_secu.sh
+++ BusquedaHadoop/busc_secu.sh
@@ -1,1 +1,17 @@
+#Instituto Tecnológico de Costa Rica
+#Escuela de Ingeniería en Computación
+#Recuperación de Información Textual
+#Proyecto 2
+#Mario Retana Rojas
+#Wei Hua Zheng Ma
 
+if [-d "busqueda_classes"]
+then
+	rm -rf busqueda_classes
+	#hadoop fs -rmr /user/cloudera/busqueda
+fi
+
+mkdir busqueda_classes
+
+javac -d busqueda_classes NumeradorArchivos.java
+java -cp busqueda_classes/ NumeradorArchivos "/home/cloudera/Hadoop-Ejemplos/man.utf8/"
